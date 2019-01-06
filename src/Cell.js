6f563988d0ed0ff564e21d0123a5222034643b37
Parker Smith <parker@insanityfarm.com>
2019-01-05 21:57:50 -0800

    Replaced messy draw mode logic with simpler bit operation

--- src/Cell.js
+++ src/Cell.js
@@ -37,42 +37,32 @@ class Cell extends Component {
     }
 
     draw() {
-
         let classes = this.state.classes;
         let drawMode = this.props.inputMode.drawMode;
-
         if(typeof drawMode === 'undefined') {
             drawMode = {
-                isSetting:  (!classes.disabled && !classes.active),
-                type:       this.props.inputMode.isModifierActive ? 'disabled' : 'active'
+                isDrawing:      (!classes.disabled && !classes.active), // true = currently drawing, false = erasing
+                isActiveType:   !this.props.inputMode.isModifierActive  // true = draw/erase active, false = disabled
             };
             this.props.setDrawMode(drawMode);
         }
-
-        // TODO: Simplify this logic, there must be a way
-        if(classes.disabled) {
-            if(!drawMode.isSetting && drawMode.type === 'disabled') {
-                // classes.active = false;
+        // fancy logic to handle draw mode logic in the most efficient way I know of
+        // https://codeburst.io/using-javascript-bitwise-operators-in-real-life-f551a731ff5
+        switch(+drawMode.isDrawing << 3 | +drawMode.isActiveType << 2 | +classes.active << 1 | +classes.disabled) {
+            case 0b0001:
                 classes.disabled = false;
-            }
-        } else {
-            if(classes.active) {
-                if(!drawMode.isSetting && drawMode.type === 'active'){
-                    classes.active = false;
-                    // classes.disabled = false;
-                }
-            } else {
-                if(drawMode.isSetting && drawMode.type === 'active') {
-                    classes.active = true;
-                    // classes.disabled = false;
-                }
-                if(drawMode.isSetting && drawMode.type === 'disabled') {
-                    // classes.active = false;
-                    classes.disabled = true;
-                }
-            }
+                break;
+            case 0b0010:
+            case 0b0110:
+                classes.active = false;
+                break;
+            case 0b1000:
+                classes.disabled = true;
+                break;
+            case 0b1100:
+                classes.active = true;
+                break;
         }
-
         this.setState({classes: classes});
     }
 }
